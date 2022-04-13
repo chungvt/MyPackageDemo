c17ec8e4ae156f75b30a8b8757c2dcce744094fe
Vuong Chung <chungvt@quangich.com>
2022-04-13 16:40:58 +0700

    add common

---
+++ Sources/MySwiftPackage/Common.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vuong Chung on 13/04/2022.
+//
+
+import Foundation
+
+public struct Common {
+    public func sayHello() {
+        print("Hello world")
+    }
+}
